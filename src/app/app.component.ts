abd6b5f7bb5d983898ed5609387d8a0a9ea4b996
ValeriiaBond <valeriaredkina19@gmail.com>
2021-08-11 20:49:08 +0300

    pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  arr = [1,1,2,3,5,8,13]
-
-  objs = [
-    {title: 'Post 1', author: 'Valeriia', comments: [
-        {name: 'Max', text: 'lorem1'},
-        {name: 'Max', text: 'lorem2'},
-        {name: 'Max', text: 'lorem3'},
-      ]},
-    {title: 'Post 2', author: 'Valeriia 2', comments: [
-        {name: 'Max 2', text: 'lorem1'},
-        {name: 'Max 2', text: 'lorem2'},
-        {name: 'Max 2', text: 'lorem3'},
-      ]}
-  ]
+  now: Date = new Date()
 }
